fc3ad6ddf913c15a57460401a56630faa0902364
Edgar De Leon <danieldeleon28@gmail.com>
2019-09-09 12:51:45 -0600

    Detalle pub y formulario agregar

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def listar_pub(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/listar_pub.html', {'posts' : posts})
+
+def detalle_pub(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detalle_pub.html', {'post': post})
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('detalle_pub', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
