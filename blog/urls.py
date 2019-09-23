fdd08567c81af1fbde7ec4c187b156d7a445c69a
Edgar De Leon <danieldeleon28@gmail.com>
2019-09-23 12:11:50 -0600

    Editar

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.listar_pub, name='listar_pub'),
     path('post/<int:pk>/', views.detalle_pub, name='detalle_pub'),
     path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
